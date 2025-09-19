c24c6e4dff84f519c64aa63adacbf89e5a4ee782
Danie <danieleah43@gmail.com>
2025-09-19 18:49:59 +0500

    dasdda

--- src/App.tsx
+++ src/App.tsx
@@ -456,7 +456,7 @@ function App() {
               <a href="#" className="text-gray-400 hover:text-white transition-colors">LinkedIn</a>
             </div>
           </div>
-        </div>dsaasd
+        </div>dsaasddasd
       </footer>
     </div>
   )
